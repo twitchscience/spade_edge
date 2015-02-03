13bfaf632e0abc70531f97a34c860d4c03fe7553
Kai Hayashi <kai.hayashi@twitch.tv>
2015-02-03 12:01:48 -0800

    made the run script continue if kafka finder fails

--- build/scripts/run_edge.sh
+++ build/scripts/run_edge.sh
@@ -6,7 +6,7 @@ cd -- "$(dirname -- "$0")"
 eval "$(curl 169.254.169.254/latest/user-data/)"
 
 KAFKA_CHROOT="/kafka/${CLOUD_DEV_PHASE:-${CLOUD_ENVIRONMENT}}"
-KAFKA_BROKERS="$(/usr/bin/kafkafinder --mode "kafka" --s3bucket "twitch-exhibitor" --exhibitorconfig "zk-exhibitor.conf" --kchroot ${KAFKA_CHROOT})"
+KAFKA_BROKERS="$(/usr/bin/kafkafinder --mode "kafka" --s3bucket "twitch-exhibitor" --exhibitorconfig "zk-exhibitor.conf" --kchroot ${KAFKA_CHROOT})" || KAFKA_BROKERS=""
 
 export HOST="$(curl 169.254.169.254/latest/meta-data/hostname)"
 export EDGE_VERSION="1"
@@ -14,6 +14,11 @@ export CROSS_DOMAIN_LOCATION="/opt/science/spade_edge/config/crossdomain.xml"
 export STATSD_HOSTPORT="localhost:8125"
 export GOMAXPROCS="4"
 
+if [ -z "${KAFKA_BROKERS}" ]
+then
+  echo "WARN: Could Not Talk to Zookeeper. Check your config."
+  echo "WARN: Continuing without Kafka."
+fi
 
 exec ../spade_edge \
   -kafka_brokers "${KAFKA_BROKERS}" \
