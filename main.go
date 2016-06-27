44be182206e4c298633bbf04e06796f415b1115f
Chad Groft <clgroft@twitch.tv>
2016-06-27 13:06:32 -0700

    Incorporate logger into main.go

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
 	"net"
 	"net/http"
 	_ "net/http/pprof"
@@ -26,6 +25,7 @@ import (
 	"github.com/twitchscience/spade_edge/requests"
 
 	"github.com/cactus/go-statsd-client/statsd"
+	"github.com/twitchscience/aws_utils/logger"
 )
 
 //spadeEdgeAuditLog defines struct of audit log in spade-edge
@@ -39,28 +39,25 @@ var (
 	statsdPrefix   = flag.String("stat_prefix", "", "statsd prefix")
 )
 
-func initStatsd(statsdHostport, prefix string) (stats statsd.Statter, err error) {
-	if len(statsdHostport) == 0 {
-		stats, _ = statsd.NewNoop()
-		log.Println("WARNING: No statsd host:port specified, disabling metric statsd!")
-	} else if len(prefix) == 0 {
-		stats, _ = statsd.NewNoop()
-		log.Println("WARNING: No statsd prefix specified, disabling metric statsd!")
-	} else {
-		if stats, err = statsd.New(statsdHostport, prefix); err != nil {
-			log.Fatalf("Statsd configuration error: %v\n", err)
-		}
+func initStatsd(statsdHostport, prefix string) (statsd.Statter, error) {
+	switch {
+	case len(statsdHostport) == 0:
+		logger.Warn("No statsd host:port specified, disabling metric statsd")
+		return statsd.NewNoop()
+	case len(prefix) == 0:
+		logger.Warn("No statsd prefix specified, disabling metric statsd")
+		return statsd.NewNoop()
+	default:
+		return statsd.New(statsdHostport, prefix)
 	}
-	return
 }
 
-func marshallingLoggingFunc(e *spade.Event) (string, error) {
-	var b []byte
+func marshallingLoggingFunc(e *spade.Event) (str string, err error) {
 	b, err := spade.Marshal(e)
-	if err != nil {
-		return "", err
+	if err == nil {
+		str = string(b)
 	}
-	return fmt.Sprintf("%s", b), nil
+	return
 }
 
 func edgeAuditLogFunc(e *spade.Event) (string, error) {
@@ -70,21 +67,22 @@ func edgeAuditLogFunc(e *spade.Event) (string, error) {
 	}
 	jsonBytes, err := json.Marshal(newAuditLog)
 	if err != nil { // create string explicitly given marshall error
-		return fmt.Sprintf("{\"UUID\":\"%s\", \"ReceiveAt\":\"%v\"}", e.Uuid, e.ReceivedAt), err
+		return fmt.Sprintf("{\"UUID\":\"%s\", \"ReceivedAt\":\"%v\"}", e.Uuid, e.ReceivedAt), err
 	}
 	return string(jsonBytes), nil
 }
 
 func main() {
+	logger.Init("info")
 	flag.Parse()
 	err := loadConfig(*configFilename)
 	if err != nil {
-		log.Fatalln("Error loading config", err)
+		logger.WithError(err).Fatal("Error loading config")
 	}
 
 	stats, err := initStatsd(os.Getenv("STATSD_HOSTPORT"), *statsdPrefix)
 	if err != nil {
-		log.Fatalf("Statsd configuration error: %v\n", err)
+		logger.WithError(err).Fatal("Statsd configuration error")
 	}
 
 	session := session.New()
@@ -94,11 +92,13 @@ func main() {
 	metadata := ec2metadata.New(session)
 	instanceID, err := metadata.GetMetadata("instance-id")
 	if err != nil {
-		log.Fatalf("Error retrieving instance-id from metadata service")
+		logger.WithError(err).Fatal("Error retrieving instance-id from metadata service")
 	}
 
 	edgeLoggers := requests.NewEdgeLoggers()
-	if config.EventsLogger != nil {
+	if config.EventsLogger == nil {
+		logger.Warn("No event logger specified")
+	} else {
 		edgeLoggers.S3EventLogger, err = loggers.NewS3Logger(
 			*config.EventsLogger,
 			config.LoggingDir,
@@ -106,13 +106,13 @@ func main() {
 			sqs,
 			s3Uploader)
 		if err != nil {
-			log.Fatalf("Error creating event logger: %v\n", err)
+			logger.WithError(err).Fatal("Error creating event logger")
 		}
-	} else {
-		log.Println("WARNING: No event logger specified!")
 	}
 
-	if config.AuditsLogger != nil {
+	if config.AuditsLogger == nil {
+		logger.Warn("No audit logger specified")
+	} else {
 		edgeLoggers.S3AuditLogger, err = loggers.NewS3Logger(
 			*config.AuditsLogger,
 			config.LoggingDir,
@@ -120,15 +120,17 @@ func main() {
 			sqs,
 			s3Uploader)
 		if err != nil {
-			log.Fatalf("Error creating audit logger: %v\n", err)
+			logger.WithError(err).Fatal("Error creating audit logger")
 		}
-	} else {
-		log.Println("WARNING: No audit logger specified!")
 	}
 
-	if config.EventStream != nil {
+	if config.EventStream == nil {
+		logger.Warn("No kinesis logger specified")
+	} else {
 		var fallbackLogger loggers.SpadeEdgeLogger = loggers.UndefinedLogger{}
-		if config.FallbackLogger != nil {
+		if config.FallbackLogger == nil {
+			logger.Warn("No fallback logger specified")
+		} else {
 			fallbackLogger, err = loggers.NewS3Logger(
 				*config.FallbackLogger,
 				config.LoggingDir,
@@ -136,24 +138,20 @@ func main() {
 				sqs,
 				s3Uploader)
 			if err != nil {
-				log.Fatalf("Error creating fallback logger: %v\n", err)
+				logger.WithError(err).Fatal("Error creating fallback logger")
 			}
-		} else {
-			log.Println("WARNING: No fallback logger specified!")
 		}
 
-		edgeLoggers.KinesisEventLogger, err = loggers.NewKinesisLogger(kinesis, *config.EventStream, fallbackLogger, stats)
+		edgeLoggers.KinesisEventLogger, err =
+			loggers.NewKinesisLogger(kinesis, *config.EventStream, fallbackLogger, stats)
 		if err != nil {
-			log.Fatalf("Error creating KinesisLogger %v\n", err)
+			logger.WithError(err).Fatal("Error creating Kinesis logger")
 		}
-	} else {
-		log.Println("WARNING: No kinesis logger specified!")
 	}
 
 	// Trigger close on receipt of SIGINT
 	sigc := make(chan os.Signal, 1)
-	signal.Notify(sigc,
-		syscall.SIGINT)
+	signal.Notify(sigc, syscall.SIGINT)
 	go func() {
 		<-sigc
 		edgeLoggers.Close()
@@ -165,14 +163,14 @@ func main() {
 	go func() {
 		err := http.ListenAndServe(net.JoinHostPort("", "81"), hystrixStreamHandler)
 		if err != nil {
-			log.Printf("Error listening to port 81 with hystrixStreamHandler %v\n", err)
+			logger.WithError(err).Error("Error listening to port 81 with hystrixStreamHandler")
 		}
 	}()
 
 	go func() {
 		err := http.ListenAndServe(net.JoinHostPort("", "8082"), http.DefaultServeMux)
 		if err != nil {
-			log.Printf("Error listening to port 8082 with http.DefaultServeMux %v\n", err)
+			logger.WithError(err).Error("Error listening to port 8082 with http.DefaultServeMux")
 		}
 	}()
 
@@ -182,9 +180,9 @@ func main() {
 		Handler:        requests.NewSpadeHandler(stats, edgeLoggers, instanceID, config.CorsOrigins),
 		ReadTimeout:    5 * time.Second,
 		WriteTimeout:   5 * time.Second,
-		MaxHeaderBytes: 1 << 20, // 0.5MB
+		MaxHeaderBytes: 1 << 20, // 1MB
 	}
-	if err := server.ListenAndServe(); err != nil {
-		log.Fatalln(err)
-	}
+
+	err = server.ListenAndServe()	// always err != nil
+	logger.WithError(err).Fatal("Error serving")
 }
