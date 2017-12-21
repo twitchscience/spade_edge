145384dde5fdcadc932d6fd79e659aa237525741
Chad Groft <clgroft@twitch.tv>
2017-12-21 15:27:10 -0800

    Remove Seeker
    Summary: We no longer need it and it's breaking our master build.
    
    Test Plan: Ran in integration without problems.
    
    Reviewers: ojcm
    
    Reviewed By: ojcm
    
    Subscribers: #scieng
    
    Differential Revision: https://phabricator.internal.justin.tv/D6001

--- build.sh
+++ build.sh
@@ -13,8 +13,6 @@ USE_PRIVATE_IP=${7:-"false"}
 export GOARCH=amd64
 export GOOS=linux
 
-SEEKER_DEBIAN="http://science.twitch.tv/debs/seeker_1.0.3_amd64.deb"
-
 bash run_tests.sh
 go install -v ./...
 gometalinter ./... --disable gocyclo --disable dupl --disable gas --deadline 30s
@@ -30,7 +28,6 @@ packer                                          \
      -var "use_private_ip=${USE_PRIVATE_IP}"    \
      -var "binary_dir"=${GOPATH}/bin            \
      -var "scripts_dir"=build/scripts           \
-     -var "seeker_debian=${SEEKER_DEBIAN}"      \
      build/packer.json | tee build.log
 
 AMIREF=`grep 'amazon-ebs,artifact,0,id,' build.log`
